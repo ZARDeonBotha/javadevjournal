281499b3b8da060f2796458baf1013c5f175a219
bothadeon <deon.botha@modusbox.com>
2022-02-01 20:45:31 +0200

    Change code to read multiple files

--- Spring-Boot-Batch/springbootbatch/src/main/java/com/javadevjournal/springbootbatch/config/SpringBatchConfig.java
+++ Spring-Boot-Batch/springbootbatch/src/main/java/com/javadevjournal/springbootbatch/config/SpringBatchConfig.java
@@ -21,7 +21,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 
